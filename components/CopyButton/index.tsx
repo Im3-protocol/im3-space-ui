c5f50b2f09122535ccbd0351a77df6e04ef2871e
ParsaBordbar <parsab71@gmail.com>
2024-09-15 18:45:29 +0300

    refactor: update CopyButton

--- components/CopyButton/index.tsx
+++ components/CopyButton/index.tsx
@@ -1,28 +1,33 @@
-'use client'
+'use client';
 import React, { useEffect, useMemo, useState } from 'react';
 import { useCopyToClipboard } from '../../hooks/useCopyToClipboard';
 import Image from 'next/image';
 
 const CopyButton = () => {
   const [isCLick, setIsClick] = useState(false);
-  const [url, setUrl] = useState('Copy Space Url')
-  
+  const [url, setUrl] = useState('Copy Space Url');
+
   useEffect(() => {
-    if (typeof window !== "undefined") {
+    if (typeof window !== 'undefined') {
       const fullUrl = window.location.href;
-      setUrl(fullUrl)
+      setUrl(fullUrl);
     }
-  }, [])
-  
+  }, []);
 
   useCopyToClipboard(url, isCLick);
 
   useEffect(() => {
-
+    setTimeout(() => {
+      setIsClick(false);
+    }, 8000);
   }, [isCLick]);
 
   const updateTextOfCopy = useMemo(() => {
-    return <p className='text-sm min-[1020px]:inline-block hidden'>{isCLick ? 'Copied' : 'Copy Space Url' }</p>;
+    return (
+      <p className="text-sm min-[1020px]:inline-block hidden">
+        {isCLick ? 'Copied' : 'Copy Space Url'}
+      </p>
+    );
   }, [isCLick]);
 
   return (
@@ -31,11 +36,9 @@ const CopyButton = () => {
       onClick={() => {
         setIsClick(true);
       }}
-    
-      className= "lk-button lk-focus-toggle-button !bg-['#373737'] md:!absolute md:!top-4 md:!left-4 md:!p-1.5 md:!px-2 "
-      
+      className="lk-button lk-focus-toggle-button !bg-['#373737'] md:!absolute md:!top-4 md:!left-4 md:!p-1.5 md:!px-2 "
     >
-      <Image alt="copy" width={16} height={16} src={'/images/copy.png'} />
+      <Image alt="copy" width={16} height={16} src={'/images/copy.svg'} />
       {updateTextOfCopy}
     </button>
   );
