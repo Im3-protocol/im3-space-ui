a1c2109e798026501009358a874c63326f8d46bc
ParsaBordbar <parsab71@gmail.com>
2024-09-15 18:46:46 +0300

    style: participentsButton compnent

--- components/ParticipentsButton/index.tsx
+++ components/ParticipentsButton/index.tsx
@@ -13,15 +13,23 @@ interface ParticipantType {
 }
 
 function ParticipantsButton({ roomName, identity }: ParticipantType) {
-  const { participants, fetchParticipants, loading } = useListParticipants(roomName);
-  const { admins, fetchAdmins } = useGetAdmins(roomName);
-  const [showList, setShowList] = useState(false);
+  const { participants, fetchParticipants, loading: loadingParticipants } = useListParticipants(roomName);
+  const { admins, fetchAdmins, loading: loadingAdmins } = useGetAdmins(roomName);
   const [isCurrentUserAdmin, setIsCurrentUserAdmin] = useState(false);
   const [isOwner, setIsOwner] = useState(false);
 
   useEffect(() => {
     fetchParticipants(roomName);
     fetchAdmins(roomName);
+
+    const intervalId = setInterval(() => {
+      fetchParticipants(roomName);
+      fetchAdmins(roomName);
+    }, 5000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
   }, [roomName]);
 
   useEffect(() => {
@@ -36,101 +44,100 @@ function ParticipantsButton({ roomName, identity }: ParticipantType) {
     }
   }, [admins, identity]);
 
-  const toggleList = () => {
-    setShowList((prevShowList) => !prevShowList);
-    fetchParticipants(roomName);
-    fetchAdmins(roomName);
-  };
-
-  return (
-    <div className="">
-      {loading ? (
-        <div className='animate-pulse'>
+  if (loadingParticipants || loadingAdmins) {
+    return (
+      <div className="side-panel px-2.5">
+        <div className="animate-pulse">
           <ParticipantBoxLazy />
           <ParticipantBoxLazy />
           <ParticipantBoxLazy />
           <ParticipantBoxLazy />
           <ParticipantBoxLazy />
         </div>
-      ) : (
-        <ul className="flex w-full flex-col gap-2 max-h-[85vh] overscroll-y-auto">
-          {/* Admin and Owner List */}
-          {admins.map((admin, index) => {
-            const isParticipantOwner = admin.identity === admins[0]?.identity;
-            const isCurrentAdmin = admin.identity === identity;
+      </div>
+    );
+  }
 
-            return (
-              <li
-                key={index}
-                className={`flex w-full py-2.5 flex-col p-3 rounded-lg bg-[#111] gap-2 justify-start items-center text-sm ${
-                  isCurrentAdmin ? 'text-yellow-400' : ''
-                }`}
-              >
-                <section className="flex w-full items-center justify-between ps-1 pb-8">
-                  <span className={`text-base ${isCurrentAdmin ? 'text-yellow-400' : ''}`}>
-                    {admin.participantName}
-                  </span>
-                  {isParticipantOwner ? (
-                    <p className="text-yellow-400 text-xs">Owner</p>
-                  ) : (
-                    <p className="text-yellow-400 text-xs">Admin</p>
-                  )}
-                </section>
+  const combinedParticipants = participants.filter(
+    (participant) => !admins.some((admin) => admin.identity === participant.identity)
+  );
 
-                {/* Admins and Owner can manage other Admins (except the Owner) */}
-                {isCurrentUserAdmin && !isParticipantOwner && (
-                  <div className="flex items-center justify-start gap-4 w-full mb-2">
-                    <MicButton roomName={roomName} identity={admin.identity} />
-                    <GrantAdminPrivilegesButton
-                      roomName={roomName}
-                      identity={admin.identity}
-                      participantName={admin.participantName}
-                    />
-                    <KickButton roomName={roomName} identity={admin.identity} />
-                  </div>
+  return (
+    <div className="side-panel px-2.5">
+      <ul className="flex w-full flex-col gap-2 max-h-[85vh] overscroll-y-auto">
+        {/* Admins List */}
+        {admins.map((admin, index) => {
+          const isCurrentAdmin = admin.identity === identity;
+          const isAdminOwner = admins[0]?.identity === admin.identity;
+
+          return (
+            <li
+              key={index}
+              className={`flex w-full flex-col p-4 rounded-lg bg-[#111] gap-2 justify-start items-center text-sm ${
+                isCurrentAdmin ? 'text-yellow-400' : ''
+              }`}
+            >
+              <section className="flex w-full items-center justify-between ps-1 pb-8">
+                <span className={`text-base ${isCurrentAdmin ? 'text-yellow-400' : ''}`}>
+                  {admin.participantName}
+                </span>
+                {isAdminOwner ? (
+                  <p className="text-yellow-400 text-xs">Owner</p>
+                ) : (
+                  <p className="text-yellow-400 text-xs">Admin</p>
                 )}
-              </li>
-            );
-          })}
+              </section>
 
-          {/* Participants List excluding Admins */}
-          {participants
-            .filter((participant: any) => !admins.some((admin) => admin.identity === participant.identity))
-            .reverse()
-            .map((participant: any, index: any) => {
-              const isCurrentParticipant = participant.identity === identity;
+              {/* Conditional rendering for Admins */}
+              {(isOwner || (isCurrentUserAdmin && !isAdminOwner)) && !isCurrentAdmin && (
+                <div className="flex items-center justify-start gap-4 w-full mb-2">
+                  <MicButton roomName={roomName} identity={admin.identity} />
+                  <GrantAdminPrivilegesButton
+                    roomName={roomName}
+                    identity={admin.identity}
+                    participantName={admin.participantName}
+                  />
+                  <KickButton roomName={roomName} identity={admin.identity} />
+                </div>
+              )}
+            </li>
+          );
+        })}
 
-              return (
-                <li
-                  key={index}
-                  className={`flex w-full py-2.5 flex-col p-3 rounded-lg bg-[#111] gap-2 justify-start items-center text-sm ${
-                    isCurrentParticipant ? 'text-yellow-400' : ''
-                  }`}
-                >
-                  <section className="flex w-full items-center justify-between ps-1 pb-8">
-                    <span className={`text-base ${isCurrentParticipant ? 'text-yellow-400' : ''}`}>
-                      {participant.name}
-                    </span>
-                    <p className="text-yellow-400 text-xs">Participant</p>
-                  </section>
+        {/* Participants List */}
+        {combinedParticipants.reverse().map((participant, index) => {
+          const isCurrentParticipant = participant.identity === identity;
 
-                  {/* Admins and Owner can manage Participants */}
-                  {(isCurrentUserAdmin || isOwner) && (
-                    <div className="flex items-center justify-start gap-4 w-full mb-2">
-                      <MicButton roomName={roomName} identity={participant.identity} />
-                      <GrantAdminPrivilegesButton
-                        roomName={roomName}
-                        identity={participant.identity}
-                        participantName={participant.name}
-                      />
-                      <KickButton roomName={roomName} identity={participant.identity} />
-                    </div>
-                  )}
-                </li>
-              );
-            })}
-        </ul>
-      )}
+          return (
+            <li
+              key={index}
+              className={`flex w-full py-2.5 flex-col p-3 rounded-lg bg-[#111] gap-2 justify-start items-center text-sm ${
+                isCurrentParticipant ? 'text-yellow-400' : ''
+              }`}
+            >
+              <section className="flex w-full items-center justify-between ps-1 pb-8">
+                <span className={`text-base ${isCurrentParticipant ? 'text-yellow-400' : ''}`}>
+                  {participant.name}
+                </span>
+                <p className="text-yellow-400 text-xs">Participant</p>
+              </section>
+
+              {/* Conditional rendering for Participants */}
+              {(isCurrentUserAdmin || isOwner) && (
+                <div className="flex items-center justify-start gap-4 w-full mb-2">
+                  <MicButton roomName={roomName} identity={participant.identity} />
+                  <GrantAdminPrivilegesButton
+                    roomName={roomName}
+                    identity={participant.identity}
+                    participantName={participant.name}
+                  />
+                  <KickButton roomName={roomName} identity={participant.identity} />
+                </div>
+              )}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
