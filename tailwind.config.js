dd64f5975aa3b389b80c1a58b08ded844b4e6626
ParsaBordbar <parsab71@gmail.com>
2024-09-15 18:52:28 +0300

    chore: update depndenceys of tailwind and its config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        'fill-available': '-webkit-fill-available',
+      },
+    },
   },
   plugins: [],
 }
