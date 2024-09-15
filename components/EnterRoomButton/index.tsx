d380166050f039a662d0dad20ecdff2c7b9f1eac
ParsaBordbar <parsab71@gmail.com>
2024-09-15 18:48:01 +0300

    style: enterRoomButton component.

--- components/EnterRoomButton/index.tsx
+++ components/EnterRoomButton/index.tsx
@@ -1,46 +1,33 @@
 import styles from "../../styles/EnterRoomButton.module.css"
 import Image from 'next/image'
+import RecorderIcon from '../../public/images/enterRoom/RecorderIcon.svg'
 import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 import { randomString, generateRoomId, encodePassphrase } from "../../lib/client-utils";
-import Link from "next/link";
-interface startMeetingButtonTypes {
-    tittle: string,
-    icon: string,
-    type: string,
-}
 
 
-const EnterRoomButton = ({tittle, icon, type}: startMeetingButtonTypes) => {
+const EnterRoomButton = () => {
     const router = useRouter();
     const [e2ee, setE2ee] = useState(false);
     const [sharedPassphrase, setSharedPassphrase] = useState(randomString(64));
 
-    const startMeeting = (type: any) => {
-    if ( e2ee && type === 'meet' ) {
+    const startMeeting = () => {
+    if (e2ee) {
         router.push(`/rooms/${generateRoomId()}#${encodePassphrase(sharedPassphrase)}`);
-    }
-    if ( type === 'meet' ) {
+    } else {
         router.push(`/rooms/${generateRoomId()}`);
-    }
-    // /space.im3.live
-    if ( e2ee && type === 'space' ) {
-        window.location.href = "https://www.space.im3.live/rooms/${generateRoomId()}#${encodePassphrase(sharedPassphrase)}";
-    }
-    if ( type === 'space' ) {
-        window.location.href = `https://www.space.im3.live/rooms/${generateRoomId()}`;
     }
     };
 
     return(
-        <button className={styles.btn} onClick={ () => startMeeting(type) } >
+        <button className={styles.btn} onClick={startMeeting}>
             <Image
-                src={icon}
+                src={RecorderIcon}
                 width={24}
                 height={24}
                 alt="Im3_Logo"
             />
-                {tittle}
+            <span className=" hover:font-bold">Start new Space</span>
         </button>
     )
 };
