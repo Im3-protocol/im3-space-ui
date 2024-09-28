38e71d94636f532ae4ab8e094986ca39d64ada44
ParsaBordbar <parsab71@gmail.com>
2024-09-28 16:46:36 +0330

    feat: udpate RecordDropDown component to work with dynamic config and useGetRecorder Hook

--- components/RecordDropDown/index.tsx
+++ components/RecordDropDown/index.tsx
@@ -1,97 +1,118 @@
 import Image from 'next/image';
-import React, { useCallback, useMemo, useState } from 'react';
-import { useEffect } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { useLayoutEffect } from 'react';
-import { useCompositeRecorder } from '../../hooks/useCompositeRecorder';
+import useGetRecordConfig from '../../hooks/useRecordConfig';
 
-const RecordDropDown = ({ setRecordType }) => {
+const RecordDropDown = ({ setRecordType, roomName }) => {
   const [isDrop, setIsDrop] = useState<boolean>(false);
-  const localRecordType = JSON.parse(localStorage.getItem('recording type'));
+  const localRecordType = JSON.parse(localStorage.getItem('recording type') || 'null');
   const [text, setText] = useState<string | boolean>(localRecordType ?? false);
 
   useLayoutEffect(() => {
-    localStorage.setItem('recording type', JSON.stringify(text));
+    if (text) {
+      localStorage.setItem('recording type', JSON.stringify(text));
+    }
   }, [text]);
+
+  const configData = useGetRecordConfig(roomName);
+  console.log('This is recordConfig', configData);
+  const isRecordRoom = configData?.recordRoom;
+  const isRecordTypes = configData?.recordTypes;
 
   const showOptionOfDrop = useCallback(() => {
     return (
       <ul
         className={`${
-          isDrop && 'opacity-100'
-        } opacity-0  transition-all duration-500 ease-in-out rounded-lg overflow-auto`}
+          isDrop ? 'opacity-100' : 'opacity-0'
+        } transition-all duration-500 ease-in-out rounded-lg overflow-auto`}
       >
         {isDrop && (
           <>
-            <li
+            {
+              isRecordTypes.includes("speaker") && (
+                <li
               onClick={() => {
                 setRecordType('speaker');
                 setIsDrop(false);
                 setText('Record Entire Room Focus Mode');
               }}
-              className="p-4 !bg-[var(--lk-control-hover-bg)] cursor-pointer hover:!bg-[var(--lk-border-color)] "
+              className="p-4 !bg-[var(--lk-control-hover-bg)] cursor-pointer hover:!bg-[var(--lk-border-color)]"
             >
               Record Entire Room Focus Mode
             </li>
-            <li
+              )
+            }
+            { isRecordTypes.includes("grid") && (
+              <li
               onClick={() => {
                 setRecordType('grid');
                 setIsDrop(false);
                 setText('Record Entire Room Grid Mode');
               }}
-              className="p-4 !bg-[var(--lk-control-hover-bg)] cursor-pointer hover:!bg-[var(--lk-border-color)] "
+              className="p-4 !bg-[var(--lk-control-hover-bg)] cursor-pointer hover:!bg-[var(--lk-border-color)]"
             >
               Record Entire Room Grid Mode
-            </li>
-            <li
+            </li>)
+            }
+            { isRecordTypes.includes("audioOnly") &&
+            (<li
               onClick={() => {
                 setRecordType('audioOnly');
                 setIsDrop(false);
                 setText('Record Entire Room Audio');
               }}
-              className="p-4 !bg-[var(--lk-control-hover-bg)] cursor-pointer hover:!bg-[var(--lk-border-color)] "
+              className="p-4 !bg-[var(--lk-control-hover-bg)] cursor-pointer hover:!bg-[var(--lk-border-color)]"
             >
               Record Entire Room Audio
-            </li>
-            <li
+            </li>)}
+            { isRecordTypes.includes("videoOnly") &&
+            (<li
               onClick={() => {
                 setRecordType('videoOnly');
                 setIsDrop(false);
                 setText('Record Entire Room Video');
               }}
-              className="p-4 !bg-[var(--lk-control-hover-bg)] cursor-pointer hover:!bg-[var(--lk-border-color)] "
+              className="p-4 !bg-[var(--lk-control-hover-bg)] cursor-pointer hover:!bg-[var(--lk-border-color)]"
             >
               Record Entire Room Video
-            </li>
-            <li
+            </li>)}
+            { isRecordTypes.includes("adminAudio") &&
+            (<li
               onClick={() => {
                 setRecordType('adminAudio');
                 setIsDrop(false);
                 setText("Record Admin's Audio");
               }}
-              className="p-4 !bg-[var(--lk-control-hover-bg)] cursor-pointer hover:!bg-[var(--lk-border-color)] "
+              className="p-4 !bg-[var(--lk-control-hover-bg)] cursor-pointer hover:!bg-[var(--lk-border-color)]"
             >
               Record Admin's Audio
-            </li>
+            </li>)}
           </>
         )}
       </ul>
     );
-  }, [isDrop]);
+  }, [isDrop, isRecordRoom, setRecordType]);
 
   const updateText = useMemo(() => {
     console.log(text);
-    return <p className="">{text ? text : 'Select recording type'}</p>;
+    return <p>{text ? text : 'Select recording type'}</p>;
   }, [text]);
 
   return (
     <div className="flex flex-col gap-4">
       <section
-        onClick={() => setIsDrop(!isDrop)}
-        className="!p-4 !flex !justify-between lk-button !bg-[var(--lk-control-hover-bg)] !w-full hover:bg-[var(--lk-border-color)] rounded-lg "
+        onClick={() => {
+          if (isRecordRoom) {
+            setIsDrop(!isDrop);
+          }
+        }}
+        className={`!p-4 !flex !justify-between lk-button !bg-[var(--lk-control-hover-bg)] !w-full ${
+          isRecordRoom ? 'hover:bg-[var(--lk-border-color)]' : 'cursor-not-allowed opacity-50'
+        } rounded-lg`}
       >
         {updateText}
         <Image
-          className={`${isDrop && 'rotate-180'} transition-all duration-200 ease-in-out`}
+          className={`${isDrop ? 'rotate-180' : ''} transition-all duration-200 ease-in-out`}
           src={'/images/arrow-down.svg'}
           width={24}
           height={24}
