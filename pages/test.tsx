074f6f22135513e98c195e8e36e6b5159ad24167
ParsaBordbar <parsab71@gmail.com>
2024-09-28 16:52:42 +0330

    feat: update roomName page to work with dynamic configs

---
+++ pages/test.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function testPage() {
+  return (
+    <>
+        <div className="tide-embedded" 
+        data-background-color="#171B26"
+        data-text-color="#ECF2FB"
+        dark-mode
+        data-space-id="2422">
+        </div>
+        <script async src="https://www.tideprotocol.xyz/embed.js"></script>
+    </>
+  )
+}
+
+export default testPage
